53006919afea49344371d2e29c7bfeb4e828f922
elpaablo <donopwhile1@gmail.com>
2024-10-11 23:48:18 +0200

    a15

--- common.mk
+++ common.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
