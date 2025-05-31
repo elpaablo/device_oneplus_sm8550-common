7f2572e253f3572ca6be4fc8997d1e10407cc130
Bruno Martins <bgcngm@gmail.com>
2025-05-31 14:35:22 +0100

    sm8550-common: Build QSPA AIDL HAL
    Change-Id: Ibd11c9d6c168aeb6a78bfc8166b1f320b0d0cfa1

--- common.mk
+++ common.mk
@@ -282,6 +282,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QSPA
+PRODUCT_PACKAGES += \
+    vendor.qti.qspa-service
+
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor # Needed by CNE app
