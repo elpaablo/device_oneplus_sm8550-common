c541d21e0b2f3bd5193b4eb570f5e09ca2c48f13
JYRRC <javixhtsweet@gmail.com>
2025-06-27 20:02:56 +0200

    salami: go AlphaDroid
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- common.mk
+++ common.mk
@@ -417,7 +417,7 @@ PRODUCT_COPY_FILES += \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/alpha/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8550-common/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
